d070609bd53d3ea6d37a070b7db2817ca82cc56b
Kevin C. Zimmerman <kevinczimmerman@gmail.com>
2020-01-29 11:11:55 -0600

    fix for dependency upgrades

--- src/linear.jl
+++ src/linear.jl
@@ -1,6 +1,6 @@
 function μ(e::Element, energies::AbstractArray{<:Number})
     u = μᵨ(e, energies)
-    AxisArray(elements[e.Z].density * u, u.axes)
+	AxisArray((elements[e.Z].density)g/cm^3 * u, u.axes)
 end
 
 μ(e::Element, energy::T) where {T<:Number} = μ(e, [energy])[1]
