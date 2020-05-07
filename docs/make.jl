4b998ff794b235135d617f022f7700757ff93d91
Zimmerman, Kevin <kzimmerman@mru.medical.canon>
2020-05-07 13:35:57 -0500

    format make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,13 @@
 using Documenter, Attenuations
 
 makedocs(;
-    modules=[Attenuations],
-    format=Documenter.HTML(),
-    pages=[
+    modules = [Attenuations],
+    format = Documenter.HTML(),
+    pages = [
         "Home" => "index.md",
     ],
-    repo="https://github.com/kczimm/Attenuations.jl/blob/{commit}{path}#L{line}",
-    sitename="Attenuations.jl",
-    assets=String[],
+    repo = "https://github.com/kczimm/Attenuations.jl/blob/{commit}{path}#L{line}",
+    sitename = "Attenuations.jl",
+    assets = String[],
 )
 deploydocs(repo = "github.com/kczimm/Attenuations.jl")
